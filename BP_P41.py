e5bd0b1feab4ccf37d3fe6ea467cd5d34593d12d
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-12 15:00:51 +0530

    Add files via upload

---
+++ BP_P41.py
@@ -0,0 +1,13 @@
+# Generate random float where the value between 5 to 50
+
+# use Python Math module
+
+# import lib.
+import random
+
+print("The Random Float number is between 5 to 50: ",
+        random.uniform(5, 50))
+
+# use uniform() function
+
+# Thanks for Watching
